771d5be4af4ba0c556174e86feb07e880d7920eb
Beenaerts Sebastian <1135552@student.khlim.be>
2013-11-21 14:55:59 +0100

    coder

--- coder.rb
+++ coder.rb
@@ -8,7 +8,11 @@ c = Matrix[[2, 3], [5, 9]] # coderingsmatrix
 a = Matrix[[29, 45], [125, 216]] # versleutelde boodschap
 b = a * c.inverse # boodschap
 
-puts numberToLetter( b[0,0] ) \
-   + numberToLetter( b[0,1] ) \
-   + numberToLetter( b[1,0] ) \
-   + numberToLetter( b[1,1] )
+puts "#{d[0,0].to_i},#{d[0,1].to_i},#{d[1,0].to_i},#{d[1,1].to_i}"
+e = c.inverse
+puts "#{e[0,0].to_i},#{e[0,1].to_i},#{e[1,0].to_i},#{e[1,1].to_i}"
+f= d*e
+puts "#{f[0,0].to_i},#{f[0,1].to_i},#{f[1,0].to_i},#{f[1,1].to_i}"
+g=a*e
+puts "#{g[0,0].to_i},#{g[0,1].to_i},#{g[1,0].to_i},#{g[1,1].to_i}"
+puts "#{numberToLetter( g[0,0].to_i)}#{numberToLetter( g[0,1].to_i)}#{numberToLetter( g[1,0].to_i)}#{numberToLetter( g[1,1].to_i)}"
